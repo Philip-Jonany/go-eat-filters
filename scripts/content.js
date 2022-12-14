97cf78a04b2a89f086a7d71b19baedc7578ad621
Philip Jonany <72227955+Philip-Jonany@users.noreply.github.com>
2022-12-13 16:25:26 -0800

    Create content.js

---
+++ scripts/content.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (body) {
+  const text = body.textContent;
+  const heading = article.querySelector("h1");
+  (heading).insertAdjacentElement("afterend", heading);
+}
